b2c227652f0c9e96d9cc4b0855ae74d8c2700434
Bunyamin Genc <bunyaming@uia.no>
2024-12-23 19:19:43 +0100

    useSTate Obje methods

--- react/rc05_events-useStateHook/src/3-useState/UseStateObject.jsx
+++ react/rc05_events-useStateHook/src/3-useState/UseStateObject.jsx
@@ -4,3 +4,16 @@
     // toggle = !toggle
     //! toggle butonuna basildiginda toggle state'nin degilini al
    
+
+    import React from 'react'
+    
+    const UseStateObject = () => {
+      return (
+        <div>
+          UseState
+        </div>
+      )
+    }
+    
+    export default UseStateObject
+    
--- react/rc05_events-useStateHook/src/App.js
+++ react/rc05_events-useStateHook/src/App.js
@@ -1,6 +1,7 @@
 // import Events from "./1-events/Events"
 // import Counter from "./2-classComponent/Counter"
 import UseStateCounter from "./3-useState/UseStateCounter"
+import UseStateObject from "./3-useState/UseStateObject"
 
 function App() {
   return (
@@ -8,7 +9,8 @@ function App() {
       {/* <Events />
       <Counter /> */}
 
-      <UseStateCounter />
+      {/* <UseStateCounter /> */}
+      <UseStateObject />
     </div>
   )
 }
