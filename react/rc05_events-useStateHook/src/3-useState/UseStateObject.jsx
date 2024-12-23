caf9907164b606e10b6f11d79967e31a0517cb28
Bunyamin Genc <bunyaming@uia.no>
2024-12-23 21:12:59 +0100

    toggle true false apen close

--- react/rc05_events-useStateHook/src/3-useState/UseStateObject.jsx
+++ react/rc05_events-useStateHook/src/3-useState/UseStateObject.jsx
@@ -3,22 +3,33 @@
 // toggle = !toggle
 //! toggle butonuna basildiginda toggle state'nin degilini al
 
-import React, { useState } from "react";
+import React, {useState} from 'react';
 
 const UseStateObject = () => {
+  // const [ name, setName] = useState("John")
+  // const [ age, setAge] = useState(30)
+  // const [ salary, setSalary] = useState(50000)
 
-// const [ name, setName] = useState("John")
-// const [ age, setAge] = useState(30)
-// const [ salary, setSalary] = useState(50000)
-
-const [kisi, setKisi ] = useState (
-  {
-    name : "Kemal" ,
+  const [kisi, setKisi] = useState ({
+    name: 'Kemal',
     age: 21,
-    salary: 10000
-  }
-)
-   
+    salary: 10000,
+  });
+
+    const [toggle, setToggle] = useState("true")
+
+  // let toggle = false;
+
+  const handleToggle = () => {
+    // toggle = !toggle;
+    setToggle(!toggle);
+
+    if (toggle) {
+      setKisi ({name: 'Mehmet Kan', age: 25, salary: 20000});
+    } else {
+      setKisi ({name: 'Kemal', age: 21, salary: 10000});
+    }
+  };
 
   return (
     <div>
@@ -28,16 +39,22 @@ const [kisi, setKisi ] = useState (
       <h2>Name:{kisi.name}</h2>
       <h2>Age:{kisi.age}</h2>
       <h2>Salary:{kisi.salary}</h2>
-{/* 
+      {/* 
       <button onClick={() => setName("Hasan")}>Change Name</button>
       <button onClick={() => setAge (age +1)}>inc age</button>
       <button onClick={() => setSalary (salary + 5000)}>inc salary</button> */}
 
-      <button onClick={() => setKisi({...kisi, name:"Mehmet Kan"})}>Change Name</button>
-      <button onClick={() => setKisi({...kisi, age: kisi.age +1})}>inc age</button>
-      <button onClick={() => setKisi({...kisi, salary: kisi.salary +3000})}>ing salary</button>
-    
+      <button onClick={() => setKisi ({...kisi, name: 'Mehmet Kan'})}>
+        Change Name
+      </button>
+      <button onClick={() => setKisi ({...kisi, age: kisi.age + 1})}>
+        inc age
+      </button>
+      <button onClick={() => setKisi ({...kisi, salary: kisi.salary + 3000})}>
+        ing salary
+      </button>
 
+      <button onClick={handleToggle}>Toggle</button>
     </div>
   );
 };
