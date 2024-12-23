60873bb2a68e4cc8a93adb144a0688b20e8e67b5
Bunyamin Genc <bunyaming@uia.no>
2024-12-23 19:27:38 +0100

    useState component

--- react/rc05_events-useStateHook/src/3-useState/UseStateObject.jsx
+++ react/rc05_events-useStateHook/src/3-useState/UseStateObject.jsx
@@ -1,19 +1,33 @@
+//! Toggle butonuna her basildiginda true-false degisen bir state tanimladik
 
-  //! Toggle butonuna her basildiginda true-false degisen bir state tanimladik
-  
-    // toggle = !toggle
-    //! toggle butonuna basildiginda toggle state'nin degilini al
-   
+// toggle = !toggle
+//! toggle butonuna basildiginda toggle state'nin degilini al
 
-    import React from 'react'
-    
-    const UseStateObject = () => {
-      return (
-        <div>
-          UseState
-        </div>
-      )
-    }
-    
-    export default UseStateObject
-    
+import React, { useState } from "react";
+
+const UseStateObject = () => {
+
+const [ Name, setName] = useState("John")
+const [ age, setAge] = useState(30)
+const [ salary, setSalary] = useState(50000)
+
+
+
+  return (
+    <div>
+
+      <h3>USE STATE OBJECT</h3>
+
+      <h2>Name:</h2>
+      <h2>Age:</h2>
+      <h2>Salary:</h2>
+
+      <button>Change Name</button>
+      <button>inc age</button>
+      <button>inc salary</button>
+
+    </div>
+  );
+};
+
+export default UseStateObject;
