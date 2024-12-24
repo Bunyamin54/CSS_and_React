44d39c8d24187def9da55d9d57ada4695de36a9b
Bunyamin Genc <bunyaming@uia.no>
2024-12-24 18:27:01 +0100

    oncChange Hook

--- react/rc06_forms-otherEvents/src/components/2-keyboardClipboardEvents/KeyboardClipboard.jsx
+++ react/rc06_forms-otherEvents/src/components/2-keyboardClipboardEvents/KeyboardClipboard.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const KeyboardClipboard = () => {
+
+ const [ content, setContent] = useState("")
+
+
+
   return (
-    <div>
-      keyboard clipboard
+
+    <div className='container'>
+     
+     
+      <h2 className='text-center'>Keyboard-Clipboard</h2>
+     
+     
+      <input type="text" className='form-control'
+
+       onChange={(e) => {
+        
+        e.target.value = e.target.value.toUpperCase()
+        
+        setContent(e.target.value)}} />
+     
+     
+     
+      <div className='mt-4'>
+        <h3>Copied Content</h3>
+        <p>{content}</p>
+      </div>
     </div>
   )
 }
