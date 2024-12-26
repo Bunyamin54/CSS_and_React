9eaf361302d9b89fb541615205f6951cd7bc06ac
Bunyamin Genc <bunyaming@uia.no>
2024-12-26 01:13:24 +0100

    tailwind css

--- react/tailwindcss-intro/src/App.js
+++ react/tailwindcss-intro/src/App.js
@@ -2,7 +2,7 @@ function  App () {
   return (
   <div>
    
-   <h1 className="text-2xl font-bold">Tailwind Css Kurulumu</h1>
+   <h1 className="text-4xl font-bold">Tailwind CSS Kurulumu</h1>
       
         <h1 className="text-3xl font-bold underline">Hello World</h1>
         </div>
