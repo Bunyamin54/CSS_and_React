a984481d3f1d457f082d6b9a77e71880ac6cc50a
Bunyamin Genc <bunyaming@uia.no>
2024-12-26 01:22:21 +0100

    layer base components

--- react/tailwindcss-intro/src/App.js
+++ react/tailwindcss-intro/src/App.js
@@ -1,11 +1,36 @@
 function  App () {
   return (
-  <div>
+  <div  className = "bg-lime-300 h-screen p-[50px]">
    
-   <h1 className="text-4xl font-bold">Tailwind CSS Kurulumu</h1>
+   <h1 className="text-8xl font-bold bg-[#bebe]">Tailwind CSS Kurulumu</h1>
       
         <h1 className="text-3xl font-bold underline">Hello World</h1>
+
+  <div className = "flex  gap-3 mt-4">
+  <div  className = "flex flex-col justify-between items-senter bg-slate-500 rounded-xl w-3/12 h-[250px] ">
+
+</div>
+
+<div  className = "flex flex-col justify-between items-senter bg-slate-500 rounded-xl w-3/12 h-[250px] ">
+  </div>
+
+    </div>
+
+
+
+
+
+
+
         </div>
+
+
+   
+
+
+
+
+
   );
  }
 
