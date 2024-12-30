e8f6a4994bcf5cd7aa66a2d650947321a8cebfd7
Bunyamin Genc <bunyaming@uia.no>
2024-12-30 12:15:13 +0100

    useEffect Hook componentDidmount

--- react/tailwindcss-intro/src/App.js
+++ react/tailwindcss-intro/src/App.js
@@ -6,7 +6,7 @@ function  App () {
       
         <h1 className="text-3xl font-bold underline">Hello World</h1>
 
-  <div className = "flex  gap-3 mt-4">
+  <div className = "flex flex-col sm:flex-row gap-3 mt-4 ">
   <div  className = "flex flex-col justify-between items-senter bg-slate-500 rounded-xl w-3/12 h-[250px] ">
   <img src="https://www.themoviedb.org/t/p/w600_and_h900_bestv2/1LRLLWGvs5sZdTzuMqLEahb88Pc.jpg" alt=""  className = "rounded-xl h-[250px] animate-pulse " />
   <p className = "line-clamp-2 hover:line-clamp-none">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus iusto qui adipisci eveniet officia nulla voluptatum praesentium, quo id neque aliquid mollitia ut suscipit a. Alias odit quae impedit sed!</p>
