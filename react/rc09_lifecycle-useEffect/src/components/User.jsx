189455d8b6c369a1173a417266ba1125043d9866
Bunyamin Genc <bunyaming@uia.no>
2024-12-30 14:22:26 +0100

    optional chaining

--- react/rc09_lifecycle-useEffect/src/components/User.jsx
+++ react/rc09_lifecycle-useEffect/src/components/User.jsx
@@ -18,12 +18,17 @@ const User = () => {
   
   return (
     <div>
-      <img src="" alt="" />
+        {/* //* optional chaining  ? varsa */}
+
+      <img src={userData?.picture?.large} className='rounded-circle' alt="" />
       <h4>Hi, My name is</h4>
-      <h3>Email</h3>
-      <h4>dob</h4>
+      <h1>{userData?.name?.first} {userData?.name?.last}
+
+      </h1>
+      <h3>{userData?.email}</h3>
+      <h4>{new Date(userData?.dob?.date).toLocaleDateString("no-NO")}</h4>
       <h5>phone</h5>
-      <h6>adress</h6>
+      <h6>address</h6>
 
 
   <button className='btn btn-success' onClick={getUser}>Get Random User</button>
