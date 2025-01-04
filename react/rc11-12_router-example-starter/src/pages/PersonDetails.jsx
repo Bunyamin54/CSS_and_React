338595bd672418aeeb1bf2e9b28e3f5fa20378fc
Bunyamin Genc <bunyaming@uia.no>
2025-01-04 19:25:07 +0100

    useNavigation

--- react/rc11-12_router-example-starter/src/pages/PersonDetails.jsx
+++ react/rc11-12_router-example-starter/src/pages/PersonDetails.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const PersonDetails = () => {
+   let {state: person} = useLocation();
+   let navigate = useNavigate();  
+   
   return (
-    <div>
-      Person Detail
-    </div>
+    
+     <div className="container text-center mt-4">
+     <h3>
+       { person?.first_name} {person?.last_name}
+</h3>
+        <img class  = "rounded" src = {person?.avatar} alt = ""  />
+         <p> {person?.email} </p>
+
+         <div> 
+
+   <button  onClick={() => navigate ("/")} className='btn btn-success me-2'>Go Home  </button>
+   <button  onClick={() => navigate ("-1")} className='btn btn-warning me-2'>Go Back  </button>
+
+         </div>
+          
+        
+   </div>
+
+ 
+   
+    
   )
 }
 
