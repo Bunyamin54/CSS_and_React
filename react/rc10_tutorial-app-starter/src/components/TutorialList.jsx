4e1ad2272f048a3f642ddff9271662ab7e2bd291
Bunyamin Genc <bunyaming@uia.no>
2025-01-05 23:45:40 +0100

    modal bootstrap

--- react/rc10_tutorial-app-starter/src/components/TutorialList.jsx
+++ react/rc10_tutorial-app-starter/src/components/TutorialList.jsx
@@ -1,6 +1,7 @@
 import { FaEdit } from "react-icons/fa"
 import { AiFillDelete } from "react-icons/ai"
 import axios from "axios"
+import Modal from "./Modal"
 
 const TutorialList = ({tutorials ,getTutorials }) => {
 
@@ -73,7 +74,9 @@ const TutorialList = ({tutorials ,getTutorials }) => {
             )
           })}
         </tbody>
-      </table>
+      </table> 
+
+       <Modal />
     </div>
   )
 }
