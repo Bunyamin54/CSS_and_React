b63c0c9b2ceac2c8191a862857ffa2f495545441
Bunyamin Genc <bunyaming@uia.no>
2025-01-05 21:37:53 +0100

    axios didmount useEffect

--- react/rc10_tutorial-app-starter/src/pages/Home.jsx
+++ react/rc10_tutorial-app-starter/src/pages/Home.jsx
@@ -1,7 +1,30 @@
 import AddTutorial from "../components/AddTutorial";
 import TutorialList from "../components/TutorialList";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 const Home = () => {
+
+  const [tutorial, setTutorial] = useState({
+   
+  })  
+
+  const BASE_URL =" https://tutorial-api.fullstack.clarusway.com/tutorials/"
+
+
+  const getTutorials = async () => {
+  const data = await axios(BASE_URL)
+  console.log(data.data)
+  }
+
+   console.log(tutorial)
+
+    //  * mount asamasinda istek atiliyor ve gelen veri setTutorial ile tutorial state'ine set ediliyor
+   useEffect(() => {
+    getTutorials()
+   }, [])
+
+
   return (
     <>
       <AddTutorial />
