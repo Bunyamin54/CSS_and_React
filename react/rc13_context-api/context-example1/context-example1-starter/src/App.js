aa30db6a3d8a4c4b7ed5150c11ad536c8e108092
Bunyamin Genc <bunyaming@uia.no>
2025-01-06 22:08:20 +0100

    useNavigate Hook

--- react/rc13_context-api/context-example1/context-example1-starter/src/App.js
+++ react/rc13_context-api/context-example1/context-example1-starter/src/App.js
@@ -7,12 +7,17 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import PersonDetail from "./pages/PersonDetail";
 import Login from "./pages/Login";
 import { LoginContext } from "./context/LoginContext";
+import { useState } from "react";
 
 function App() {
+
+  const [user, setUser] = useState({ email: "", password: "" });
+
   return (
 
       //   * uyuglamayi sarmaladik
-    <LoginContext.Provider value={{}}> 
+
+    <LoginContext.Provider value={{user, setUser}}> 
      <BrowserRouter>
       <Navs />
       <Routes>
